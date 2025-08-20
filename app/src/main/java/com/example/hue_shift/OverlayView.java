f43fd0216259220583a524fae13e76722af7b819
Alaaeddine ZORGANI <zorganialaaeddine@gmail.com>
2025-08-20 04:08:04 +0100

    added OverlayView class

--- app/src/main/java/com/example/hue_shift/OverlayView.java
+++ app/src/main/java/com/example/hue_shift/OverlayView.java
@@ -1,4 +1,31 @@
 package com.example.hue_shift;
 
-public class OverlayView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class OverlayView extends View {
+    private int filterColor;
+    private final Paint paint;
+
+    public OverlayView(Context context, int initalIntensity) {
+        super(context);
+        paint = new Paint();
+        setFilterIntensity(initalIntensity);
+    }
+
+    private void setFilterIntensity(int intensity) {
+        // slider's 0-100 to alpha 0-255
+        int alpha = (int) (intensity * 2.55);
+        filterColor = Color.argb(alpha, 255, 85, 0);
+        invalidate(); // redraw with new color
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawColor(filterColor);
+    }
 }
