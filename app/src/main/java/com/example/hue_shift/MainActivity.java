b3220039fda51f453641bafdb668dd5840e25bdb
Alaaeddine ZORGANI <zorganialaaeddine@gmail.com>
2025-08-20 04:47:05 +0100

    added the MainActivity class + fixed typo

--- app/src/main/java/com/example/hue_shift/MainActivity.java
+++ app/src/main/java/com/example/hue_shift/MainActivity.java
@@ -1,24 +1,62 @@
 package com.example.hue_shift;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.provider.Settings;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button toggleButton;
+    private SeekBar intensitySlider;
+    private boolean isFilterOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        toggleButton = findViewById(R.id.toggleButton);
+        intensitySlider = findViewById(R.id.intensitySlider);
+
+        intensitySlider.setProgress(50);
+
+        if (!Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName())
+            );
+            startActivity(intent);
+            Toast.makeText(this, "Please grant overlay permission", Toast.LENGTH_LONG).show();
+        }
+
+        toggleButton.setOnClickListener(v -> {
+            if (isFilterOn) {
+                stopService(new Intent(this, FilterService.class));
+                toggleButton.setText("Turn ON");
+                isFilterOn = false;
+            } else {
+                startService(new Intent(this, FilterService.class));
+                toggleButton.setText("Turn OFF");
+                isFilterOn = true;
+            }
+        });
+
+        intensitySlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                FilterService.setFilterIntensity(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
         });
     }
 }
